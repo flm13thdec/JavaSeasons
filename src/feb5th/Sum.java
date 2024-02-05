d1aaf7b4a560832e41625fcaa223d5b174bbdcaa
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-02-05 22:07:30 +0530

    Java Programs till Feb 5th

---
+++ src/feb5th/Sum.java
@@ -0,0 +1,21 @@
+package feb5th;
+
+public class Sum {
+	
+	public void add(int x,int y)
+	{
+		System.out.println("Sum is "+(x+y));
+	}
+	
+	
+	public void add(double x,double y)
+	{
+		System.out.println("Sum is "+(x+y));
+	}
+	
+	public void add(long x,long y)
+	{
+		System.out.println("Sum is "+(x+y));
+	}
+
+}
